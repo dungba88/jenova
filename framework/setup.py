0d6269020fd0940aa41774851e9a0291d2d6dab5
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-14 23:20:22 +0800

    change metadata

--- framework/setup.py
+++ framework/setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='EV3 Bot Framework',
+setup(name='ev3-bot',
       version='0.1',
       description='Framework for creating EV3 Bot',
       author='Anh Dung Bui',
