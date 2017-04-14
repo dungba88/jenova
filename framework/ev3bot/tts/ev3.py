55098c3f895773133fa74f2d9f4cfa82991821ea
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-15 01:29:56 +0800

    Add wrapper for ev3dev TTS

--- framework/ev3bot/tts/ev3.py
+++ framework/ev3bot/tts/ev3.py
@@ -1,5 +1,11 @@
 """TTS Engine by EV3"""
 
+from ev3dev import ev3
+
 class EV3TTSEngine(object):
     """Wrapper class for EV3 TTS"""
-    pass
+    def say(self, texts):
+        """Speak the texts"""
+        for text in texts:
+            ev3.Sound.speak(text)
+            
