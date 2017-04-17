271f74e1bc1d55e9aa4c969517a1342c0b0bd162
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-17 23:10:03 +0800

    refactor classes

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,9 @@ from app import APP_INSTANCE as app
 from bootstrap import ApplicationBootstrap
 
 app.bootstrap = ApplicationBootstrap()
-app.run()
 
 # export WSGI application
 application = app.api
+
+# run the application
+app.run()
