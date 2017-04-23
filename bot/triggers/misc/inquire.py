303939f403a804d7512496749698dc2bd7d06833
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-23 16:08:07 +0800

    add triggers

---
+++ bot/triggers/misc/inquire.py
@@ -0,0 +1,20 @@
+"""Trigger implementation for inquiring"""
+
+import logging
+
+from app import APP_INSTANCE as app
+from utils import tts
+
+LOGGER = logging.getLogger(__name__)
+
+def run(execution_context):
+    """run the action"""
+    event_name = execution_context.event_name.split('.')[1]
+    config_name = 'behavior.inquire.' + event_name
+    reacts = app.get_config(config_name)
+    txt_no_data = app.get_config('behavior.inquire.no_data')
+    if reacts is None or len(reacts) == 0:
+        LOGGER.warning('No behavior configured for ' + config_name)
+        tts.say_random(txt_no_data)
+        return
+    tts.say_random(reacts)
--- bot/triggers/misc/react.py
+++ bot/triggers/misc/react.py
@@ -1,10 +1,11 @@
-"""Trigger implementation for greeting"""
+"""Trigger implementation for reacting"""
 
 from app import APP_INSTANCE as app
 from utils import tts
 
 def run(execution_context):
     """run the action"""
-    config_name = 'behavior.react.' + execution_context.event_name
+    event_name = execution_context.event_name
+    config_name = 'behavior.react.' + event_name.split('.')[1]
     reacts = app.get_config(config_name)
     tts.say_random(reacts)
