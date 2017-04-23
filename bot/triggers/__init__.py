880dc5dec59c422b7e6c68b59d2b2d9c8d6b1e74
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-23 16:07:41 +0800

    refactor trigger

--- bot/triggers/__init__.py
+++ bot/triggers/__init__.py
@@ -6,6 +6,7 @@ from triggers.commands.stop import run as stop
 from triggers.commands.story import run as story
 from triggers.commands.story import stop_story as stop_story
 from triggers.misc.react import run as react
+from triggers.misc.inquire import run as inquire
 from ev3bot.trigger import register_trigger
 
 def init_all_triggers(manager):
@@ -13,6 +14,6 @@ def init_all_triggers(manager):
     register_trigger(manager, event_name='sing', action=sing)
     register_trigger(manager, event_name='say', action=say)
     register_trigger(manager, event_name='stop', action=stop)
-    register_trigger(manager, event_name='greetings', action=react)
-    register_trigger(manager, event_name='thank', action=react)
-    register_trigger(manager, event_name='inquire.story', action=story, stop_action=stop_story)
+    register_trigger(manager, event_name='tell_story', action=story, stop_action=stop_story)
+    register_trigger(manager, event_name='react.*', action=react)
+    register_trigger(manager, event_name='inquire.*', action=inquire)
