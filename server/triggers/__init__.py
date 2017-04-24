d2abbc9a172a498f9e67addf5878f768fd756e96
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-24 20:04:28 +0800

    refactor

--- server/triggers/__init__.py
+++ server/triggers/__init__.py
@@ -1,12 +1,12 @@
 """Custom triggers"""
 
 from ev3bot.trigger import register_trigger
-from triggers.add_input import run as add_input
-from triggers.train import run as train
-from triggers.test import run as test
+from triggers import add_input
+from triggers import train
+from triggers import test
 
 def init_all_triggers(manager):
     """initialize all triggers"""
-    register_trigger(manager, event_name='add_input', action=add_input)
-    register_trigger(manager, event_name='train', action=train)
-    register_trigger(manager, event_name='test', action=test)
+    register_trigger(manager, event_name='add_input', action=add_input.run)
+    register_trigger(manager, event_name='train', action=train.run)
+    register_trigger(manager, event_name='test', action=test.run)
