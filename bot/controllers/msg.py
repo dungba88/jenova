3a87efd311591e8adde6f0550288c264f468894d
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-25 21:50:47 +0800

    refactor

--- bot/controllers/msg.py
+++ bot/controllers/msg.py
@@ -8,21 +8,27 @@ class MessageResource(object):
 
     def on_post(self, req, res):
         """Handle POST method"""
+        res.set_header('Access-Control-Allow-Origin', '*')
+        res.set_header("Access-Control-Expose-Headers", "Access-Control-Allow-Origin")
+        res.set_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
         msg = encode.decode_from_request(req)
         if msg is None:
             raise ValueError('msg cannot be empty')
 
-        msg_name = msg.get('name', None)
+        msg_name = msg.get('name')
         msg_args = msg.get('args', dict())
 
-        if msg_name is None:
-            raise ValueError('name cannot be empty')
+        try:
+            result = app.trigger_manager.fire(msg_name, msg_args)
+            result = {
+                'status': 0,
+                'msg': result
+            }
+        except Exception as ex:
+            result = {
+                'status': 1,
+                'msg': type(ex).__name__ + ': ' + str(ex)
+            }
 
-        app.trigger_manager.fire(msg_name, msg_args)
-
-        result = {
-            'status': 0,
-            'message': msg_name
-        }
         res.body = encode.encode(result)
