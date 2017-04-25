042cf9971fd42af3e58ed1867c3cf56e058aeac9
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-25 21:52:38 +0800

    add trigger

--- server/triggers/__init__.py
+++ server/triggers/__init__.py
@@ -4,9 +4,11 @@ from ev3bot.trigger import register_trigger
 from triggers import add_input
 from triggers import train
 from triggers import test
+from triggers import say
 
 def init_all_triggers(manager):
     """initialize all triggers"""
     register_trigger(manager, event_name='add_input', action=add_input.run)
     register_trigger(manager, event_name='train', action=train.run)
     register_trigger(manager, event_name='test', action=test.run)
+    register_trigger(manager, event_name='say', action=say.run)
