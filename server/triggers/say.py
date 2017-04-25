515b27c2cf479144455f1057c178ec5d4a2c5651
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-25 21:51:12 +0800

    add trigger

---
+++ server/triggers/say.py
@@ -0,0 +1,17 @@
+"""Make the bot say"""
+
+from app import APP_INSTANCE as app
+from utils import http
+
+def run(execution_context):
+    """run the action"""
+    text = execution_context.event.get('text')
+    url = app.get_config('bot.url')
+    msg = {
+        'name': 'say',
+        'args': {
+            'text': text
+        }
+    }
+    http.call(url, msg)
+    
