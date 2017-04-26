321867c08834f93cbe7bcc5ee5b87ad8747a1336
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-26 21:29:26 +0800

    add facts

--- bot/utils/dynamic_facts.py
+++ bot/utils/dynamic_facts.py
@@ -1,12 +1,13 @@
 """dynamic facts"""
 
+import time
 import random
 
 from app import APP_INSTANCE as app
-from ev3dev.core import PowerSupply
 
 def get_battery_level():
     """get battery level"""
+    from ev3dev.core import PowerSupply
     supply = PowerSupply()
     return supply.measured_voltage / supply.max_voltage * 100
 
@@ -15,3 +16,15 @@ def get_health_react():
     health = app.get_config('facts.health')
     reacts = app.get_config('behavior.health_react.' + health)
     return reacts[random.randint(0, len(reacts) - 1)]
+
+def get_today():
+    """get today"""
+    return time.strftime("%x")
+
+def get_time():
+    """get current time"""
+    return time.strftime("%X")
+
+def get_day():
+    """get today's day"""
+    return time.strftime("%A")
