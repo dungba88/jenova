d55b62f3ff62e7477e9a914d8d788c175fe9c353
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-04-26 21:26:47 +0800

    add trigger

--- server/triggers/__init__.py
+++ server/triggers/__init__.py
@@ -5,6 +5,7 @@ from triggers import add_input
 from triggers import train
 from triggers import test
 from triggers import say
+from triggers import reload_conf
 
 def init_all_triggers(manager):
     """initialize all triggers"""
@@ -12,3 +13,4 @@ def init_all_triggers(manager):
     register_trigger(manager, event_name='train', action=train.run)
     register_trigger(manager, event_name='test', action=test.run)
     register_trigger(manager, event_name='say', action=say.run)
+    register_trigger(manager, event_name='reload', action=reload_conf.run)
