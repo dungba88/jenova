6264411b096c2efea5116af2e7cee8f29f41219f
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-05-01 12:06:41 +0800

    refactor

--- server/utils/learn/persist.py
+++ server/utils/learn/persist.py
@@ -13,7 +13,19 @@ def load_model_file(data_name):
     """load vocab from file"""
     return joblib.load('cache/data/' + data_name + '/model.pkl')
 
+def load_vectorizer(data_name):
+    """load vocab from file"""
+    return joblib.load('cache/data/' + data_name + '/vectorizer.pkl')
+
 def get_data_config(data_name):
     """get data config"""
     with open('cache/data/' + data_name + '/config.json') as config_file:
         return json.load(config_file)
+
+def save_model(data_name, model):
+    """save the trained model to file"""
+    joblib.dump(model, 'cache/data/' + data_name + '/model.pkl')
+
+def save_vectorizer(data_name, vectorizer):
+    """save the trained model to file"""
+    joblib.dump(vectorizer, 'cache/data/' + data_name + '/vectorizer.pkl')
