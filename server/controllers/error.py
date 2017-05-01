997784446ad691ca038230ee7779ce10c5138453
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-05-01 12:07:16 +0800

    add error handler

--- server/controllers/error.py
+++ server/controllers/error.py
@@ -1,6 +1,24 @@
 """Error handling"""
 
+import logging
+import traceback
+
 from ev3bot import encode
+
+class DefaultErrorHandler(object):
+    """default error handler class"""
+
+    def handle_error(self, ex):
+        """handle error"""
+        default_error_handler(ex)
+
+def default_error_handler(ex):
+    """Error handler for all exceptions"""
+    logger = logging.getLogger(__name__)
+    traces = traceback.format_exception(None, # <- type(e) by docs, but ignored
+                                        ex, ex.__traceback__)
+    for trace in traces:
+        logger.error(trace)
 
 def value_error_handler(ex, req, resp, params):
     """Error handler for ValueError"""
