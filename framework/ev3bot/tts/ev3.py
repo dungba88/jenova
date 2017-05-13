4a59f3385bd7c9c145ce01065174041beafa8b5e
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-05-13 23:31:44 +0800

    wait for sound

--- framework/ev3bot/tts/ev3.py
+++ framework/ev3bot/tts/ev3.py
@@ -6,5 +6,4 @@ class EV3TTSEngine(object):
         """Speak the texts"""
         from ev3dev import ev3
         for text in texts:
-            ev3.Sound.speak(text)
-            
+            ev3.Sound.speak(text).wait()
