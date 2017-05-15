326d0bf60ef96adb37a4d1588a099b0c12fe90d0
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-05-15 15:45:39 +0800

    fix bug

--- bot/utils/ev3/__init__.py
+++ bot/utils/ev3/__init__.py
@@ -14,3 +14,10 @@ def access_large_motors(motors):
 def convert_to_pos(motor, rotation):
     """convert rotation to tacho count"""
     return int(rotation * motor.count_per_rot)
+
+def calc_relative_rotation(rotation, left_power, right_power):
+    """calibrate the relative rotation with respect to power"""
+    max_power = max(abs(left_power), abs(right_power))
+    if max_power == 0:
+        return 0, 0
+    return rotation * left_power / max_power, rotation * right_power / max_power
--- bot/utils/ev3/move_tank.py
+++ bot/utils/ev3/move_tank.py
@@ -1,6 +1,6 @@
 """move both motor the equally"""
 
-from . import is_stopped, access_large_motors, convert_to_pos
+from . import is_stopped, access_large_motors, convert_to_pos, calc_relative_rotation
 
 class AbstractMoveTank(object):
     """abstract class for move tank"""
@@ -22,6 +22,12 @@ class AbstractMoveTank(object):
         self.left.wait(is_stopped)
         self.right.wait(is_stopped)
 
+    def run_tank_rotation(self, rot):
+        """run both motor with rotation"""
+        left_rot, right_rot = calc_relative_rotation(rot, self.left.speed_sp, self.right.speed_sp)
+        self.left.run_to_rel_pos(position_sp=convert_to_pos(self.left, left_rot))
+        self.right.run_to_rel_pos(position_sp=convert_to_pos(self.right, right_rot))
+
     def do_run(self, **kwargs):
         """run the actual command"""
 
@@ -37,14 +43,12 @@ class RotationMoveTank(AbstractMoveTank):
     """class for move by rotation"""
 
     def do_run(self, **kwargs):
-        rotation = kwargs['rotation']
-        self.left.run_to_rel_pos(position_sp=convert_to_pos(self.left, rotation))
-        self.right.run_to_rel_pos(position_sp=convert_to_pos(self.right, rotation))
+        rot = kwargs['rotation']
+        self.run_tank_rotation(rot)
 
 class DegreeMoveTank(AbstractMoveTank):
     """class for move by rotation"""
 
     def do_run(self, **kwargs):
-        rotation = kwargs['degree'] / 180
-        self.left.run_to_rel_pos(position_sp=convert_to_pos(self.left, rotation))
-        self.right.run_to_rel_pos(position_sp=convert_to_pos(self.right, rotation))
+        rot = kwargs['degree'] / 180
+        self.run_tank_rotation(rot)
