1eb375aa14c544f559145ba36dbf56b6431ded8a
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-05-15 18:46:34 +0800

    support remote control

---
+++ bot/utils/ev3/sensors/remote.py
@@ -0,0 +1,13 @@
+"""remote controller"""
+
+class RemoteControlReadings(object):
+    """class for accessing remote controller"""
+    def __init__(self, address):
+        from ev3dev.core import RemoteControl, InfraredSensor
+        self.sensor = RemoteControl(InfraredSensor(address=address))
+
+    def read_value(self):
+        """read the sensor value"""
+        return {
+            'buttons_pressed': self.sensor.buttons_pressed
+        }
