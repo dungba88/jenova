d7697ff7e8f1e60f93432a9c26666d2887d3f38a
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-05-15 23:41:55 +0800

    go with timed

--- bot/triggers/ev3/go.py
+++ bot/triggers/ev3/go.py
@@ -58,10 +58,16 @@ class GoBase(Trigger):
 
 class GoEV3(GoBase):
     """class for remote control"""
+    time = 1
 
     def run(self, execution_context):
         self.motors = self.app_context.get_config('ev3.motors.wheels')
         power = execution_context.event['power']
         behavior = execution_context.event['behavior']
         execution_context.finish(behavior)
+        self.time = execution_context.event['time']
         self.run_behavior(behavior, power)
+
+    def run_motor(self, motor, power):
+        """run a large motor"""
+        move_large.TimedMoveLarge(self.motors[motor]).run(time=self.time, power=power)
