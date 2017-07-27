1736254a5050bfa0a00071a6b3b115100d905a8c
Dzung Bui <dungba.sg@gmail.com>
2017-07-27 23:49:53 +0800

    refactor method

--- vision/vision_utils/object_detection/detect.py
+++ vision/vision_utils/object_detection/detect.py
@@ -23,23 +23,33 @@ def detect_classes(net, img, cfg):
                           cls_scores[:, np.newaxis])).astype(np.float32)
 
         # use non-maximum suppression to remove overlapping boxes
-        keep = nms(dets, nms_thresh)
-        dets = dets[keep, :]
+        dets = suppress_overlap(dets, nms_thresh)
 
+        # only take the boxes with sufficient score
         inds = np.where(dets[:, -1] >= conf_thresh)[0]
 
         if len(inds) > 0:
-            detection_boxes = []
-            for i in inds:
-                coord = dets[i, :4]
-                score = dets[i, -1]
-                detection_boxes.append({
-                    'coord': coord.tolist(),
-                    'score': score.item()
-                })
+            detection_boxes = map_to_detection_boxes(dets, inds)
             result.append({
                 'class': cls_ind,
                 'boxes': detection_boxes
             })
 
     return result
+
+def suppress_overlap(dets, nms_thresh):
+    """suppress overlapping boxes"""
+    keep = nms(dets, nms_thresh)
+    return dets[keep, :]
+
+def map_to_detection_boxes(dets, inds):
+    """map matrix to detection boxes"""
+    detection_boxes = []
+    for i in inds:
+        coord = dets[i, :4]
+        score = dets[i, -1]
+        detection_boxes.append({
+            'coord': coord.tolist(),
+            'score': score.item()
+        })
+    return detection_boxes
