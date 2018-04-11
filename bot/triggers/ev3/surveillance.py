31a4b8815db98efcde8b78d936a6b2fdaae71324
Dung Bui <dungba.sg@gmail.com>
2018-04-11 13:18:28 +0000

    implement surveillance navigation

--- bot/triggers/ev3/surveillance.py
+++ bot/triggers/ev3/surveillance.py
@@ -1,8 +1,10 @@
 """Remote controlling EV3"""
 
+import time
+
 from utils.ev3.sensors.infrared import InfraredSensorReadings
 from utils.navigation.sweeper import BFSSweeper
-from .go import GoEV3
+from .go import GoBase
 
 class Surveillance(object):
     """class for remote control"""
@@ -17,7 +19,8 @@ class Surveillance(object):
         infrared_sensor = self.find_infrared(sensors)
 
         self.stop()
-        self.sweeper = BFSSweeper(SimpleRobot(power, motors, infrared_sensor['port']))
+        self.sweeper = BFSSweeper(SimpleRobot(25, motors, infrared_sensor['port']))
+        self.sweeper.spiral = False
         self.sweeper.loggable = True
         self.sweeper.sweep()
 
@@ -33,27 +36,38 @@ class Surveillance(object):
                 return sensor
         return None
 
+from utils.ev3 import move_large
+
+class SurveillanceGo(GoBase):
+
+    def run_motor(self, motor, power):
+        move_large.RotationMoveLarge(self.motors[motor]).run(power=power, rotation=1.25)
+
 class SimpleRobot(object):
     """simple implementation of robot"""
 
     def __init__(self, power, motors, infrared_port):
         self.power = power
-        self.controller = GoEV3()
-        self.controller.time = 0.5
+        self.controller = SurveillanceGo()
         self.controller.motors = motors
         self.infrared_reader = InfraredSensorReadings(infrared_port)
 
     def turn_left(self):
-        self.controller.turn_left(self.power)
+        self.controller.hard_turn_left(self.power)
+        time.sleep(2)
         return True
 
     def turn_right(self):
-        self.controller.turn_right(self.power)
+        self.controller.hard_turn_right(self.power)
+        time.sleep(2)
         return True
 
     def move(self):
         infrared_readings = self.infrared_reader.read_value()
-        if infrared_readings['proximity'] < 0.5:
+        print(infrared_readings['proximity'])
+        if infrared_readings['proximity'] < 30:
+            print('walled')
             return False
         self.controller.skate_up(self.power)
+        time.sleep(2)
         return True
